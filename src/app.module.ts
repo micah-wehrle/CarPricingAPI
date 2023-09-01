c39bbf40d47a709aefeb8c2ff70c7e1b52a6b213
micah.wehrle <micah23@gmail.com>
2023-08-31 20:06:24 -0400

    File structure generation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ReportModule } from './report/report.module';
+import { ReportsController } from './reports/reports.controller';
+import { ReportsService } from './reports/reports.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, ReportModule],
+  controllers: [AppController, ReportsController],
+  providers: [AppService, ReportsService],
 })
 export class AppModule {}
