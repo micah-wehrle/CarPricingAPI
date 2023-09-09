f817038466838d3d8e817aef6f18d30c180c76c5
micah.wehrle <micah23@gmail.com>
2023-09-08 20:58:44 -0400

    more project setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ReportModule } from './report/report.module';
-import { ReportsController } from './reports/reports.controller';
-import { ReportsService } from './reports/reports.service';
+import { ReportController } from './report/report.controller';
+import { ReportService } from './report/report.service';
+import { User } from './user/user.entity';
+import { Report } from './report/report.entity';
 
 @Module({
-  imports: [UserModule, ReportModule],
-  controllers: [AppController, ReportsController],
-  providers: [AppService, ReportsService],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: [User, Report],
+    synchronize: true,
+  }), 
+  UserModule, ReportModule],
+  controllers: [AppController, ReportController],
+  providers: [AppService, ReportService],
 })
 export class AppModule {}
